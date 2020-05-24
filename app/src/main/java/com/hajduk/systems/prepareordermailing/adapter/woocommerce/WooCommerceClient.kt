98914bb4f4f991f77b2332d3611865507397f758
Iwo Hajduk <iwo.hajduk@allegro.pl>
2020-05-24 15:29:50 +0200

    cr fixes

--- app/src/main/java/com/hajduk/systems/prepareordermailing/adapter/woocommerce/WooCommerceClient.kt
+++ app/src/main/java/com/hajduk/systems/prepareordermailing/adapter/woocommerce/WooCommerceClient.kt
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.Response
@@ -74,8 +76,8 @@ class WooCommerceClient(
                 configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
                 registerModule(KotlinModule())
-//                registerModule(JavaTimeModule())
-//                registerModule(Jdk8Module())
+                registerModule(JavaTimeModule())
+                registerModule(Jdk8Module())
             }
         }
 
--- app/src/main/java/com/hajduk/systems/prepareordermailing/adapter/woocommerce/model/OrderDto.kt
+++ app/src/main/java/com/hajduk/systems/prepareordermailing/adapter/woocommerce/model/OrderDto.kt
@@ -1,13 +1,9 @@
 package com.hajduk.systems.prepareordermailing.adapter.woocommerce.model
 
-import com.fasterxml.jackson.annotation.JsonFormat
 import java.math.BigDecimal
-import java.time.LocalDateTime
 
 class OrderDto(
     val id: Int,
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-//    val dateCreated: LocalDateTime,
     val customerId: Int,
     val total: BigDecimal,
     val lineItems: List<LineItemDto> = ArrayList()
