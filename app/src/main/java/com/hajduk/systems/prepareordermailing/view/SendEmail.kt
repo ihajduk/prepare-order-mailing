6eb8f884a9b55c0ad7540aa4ac091c9cf0927037
Patryk Hajduk <patryk.k.hajduk@gmail.com>
2020-05-30 20:26:21 +0200

    Fix PR comments and add a few details enhance

--- app/src/main/java/com/hajduk/systems/prepareordermailing/view/SendEmail.kt
+++ app/src/main/java/com/hajduk/systems/prepareordermailing/view/SendEmail.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.hajduk.systems.prepareordermailing.R
 import com.hajduk.systems.prepareordermailing.adapter.email.EmailData
@@ -21,7 +22,14 @@ class SendEmail : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_email)
         findViewById<Button>(R.id.restartFlowButton).setOnClickListener { startActivity(Intent(this, ScanBarCode::class.java)) }
-        emailSender.sentEmail(resolveEmailData(), this)
+        sendEmail()
+    }
+
+    private fun sendEmail() {
+        val result = emailSender.sentEmail(resolveEmailData(), this)
+        if (!result.isSuccess()) {
+            Toast.makeText(this, "Nie udało się wysłać wiadomości: ${result.errorMessage}", Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun resolveEmailData(): EmailData {
